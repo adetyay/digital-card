90d9d293ceb66e532d2673b0ed5802bd8e94eb9c
aditya <129599872+adityax4@users.noreply.github.com>
2024-07-01 12:06:38 +0530

    add interests section

--- components/Interests.jsx
+++ components/Interests.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Interests() {
   return (
-    <div>Interests</div>
+    <div className='interests'>
+      <h1>Interests</h1>
+      <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+    </div>
   )
 }
 
