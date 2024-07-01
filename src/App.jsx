4457911f7584e2793c0b77a83346ca113e995c02
aditya <129599872+adityax4@users.noreply.github.com>
2024-07-01 09:23:31 +0530

    add font and css reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import Info from '../components/Info'
+import About from '../components/About'
+import Interests from '../components/Interests'
+import Footer from '../components/Footer'
 import './App.css'
 
 function App() {
