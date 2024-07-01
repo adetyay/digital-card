2e77faf1973789ddf9f9241f199deab8e621e5fa
aditya <129599872+adityax4@users.noreply.github.com>
2024-07-01 12:35:10 +0530

    add footer section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <i class="fa-brands fa-github"></i>
+      <i class="fa-brands fa-discord"></i>
+      <i class="fa-brands fa-instagram"></i>
+      <i class="fa-brands fa-spotify"></i>
+    </div>
   )
 }
 
