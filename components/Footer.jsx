5a335175c335de288df197358e2ffd2efddc6b7c
aditya <129599872+adityax4@users.noreply.github.com>
2024-07-01 14:29:16 +0530

    fix footer item

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='footer'>
-      <a href="https://github.com/adityax4"><i class="fa-brands fa-github"></i></a>
+      <a href="https://github.com/adityax4" target='_blank'><i class="fa-brands fa-github"></i></a>
       <a href="#"><i class="fa-brands fa-discord"></i></a>
       <a href="#"><i class="fa-brands fa-instagram"></i></a>
       <a href="#"><i class="fa-brands fa-spotify"></i></a>
